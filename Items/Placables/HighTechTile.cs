ccd9bb1808aa222933ea82a7a03e4c7d5f668698
Witherking25 <witherking@withertech.com>
2020-03-26 11:51:30 -0500

    Added A Tile

---
+++ Items/Placables/HighTechTile.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+// If you are using c# 6, you can use: "using static Terraria.Localization.GameCulture;" which would mean you could just write "DisplayName.AddTranslation(German, "");"
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace CheatyToolsReloaded.Items.Placables
+{
+	public class HighTechTile : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("A decorative building block that looks cool. Plus you can flex that you built your base out of something unobtainable.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 12;
+			item.height = 12;
+			item.maxStack = 999;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = 1;
+			item.consumable = true;
+			item.createTile = TileType<Tiles.HighTechTile>();
+		}
+	}
+}
---
+++ Tiles/HighTechTile.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace CheatyToolsReloaded.Tiles
+{
+	public class HighTechTile: ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileLighted[Type] = true;
+			drop = ItemType<Items.Placables.HighTechTile>();
+			AddMapEntry(new Color(0, 255, 0));
+		}
+	}
+}
